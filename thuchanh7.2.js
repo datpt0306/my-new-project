96e6a32b46af247c475645f151a3000e9fa29124
datpt0306 <mr.datpt@gmail.com>
2021-03-09 14:48:04 +0700

    Up bai tap 7.1.2

---
+++ thuchanh7.2.js
@@ -0,0 +1,7 @@
+let inputwidth=prompt("nhập chiều rộng")
+let inputheight=prompt("nhập chiều cao")
+let width = parseInt(inputwidth)
+let height = parseInt(inputheight)
+let area=width*height
+document.write("diện tích="+area)
+
